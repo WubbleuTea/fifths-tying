707312b22309d10068a755ab6e6a6c0393d0c3f6
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-01-22 07:25:09 -0600

    toasts done

--- 27-toast-notification/script.js
+++ 27-toast-notification/script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById('btn');
-const toasts = document.getElementById('toasts');
+const toastContainer = document.getElementById('toasts');
 
 const messages = [
   'Message One',
@@ -10,4 +10,28 @@ const messages = [
   'Message Six'
 ];
 
-button.addEventListener('click');
+const types = ['info', 'success', 'error'];
+
+button.addEventListener('click', () => createNotification());
+
+function createNotification(message = null, type = null) {
+  const notif = document.createElement('div');
+  notif.classList.add('toast');
+  notif.classList.add(type ? type : getRandomType());
+
+  notif.innerText = getRandomMessage();
+
+  toastContainer.appendChild(notif);
+
+  setTimeout(() => {
+    notif.remove();
+  }, 3000);
+}
+
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)];
+}
+
+function getRandomType() {
+  return types[Math.floor(Math.random() * types.length)];
+}
