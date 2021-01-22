e1137ba6539f6e584696d5917ff1ece5c0aaefe2
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-01-22 07:16:06 -0600

    UI done

---
+++ 27-toast-notification/script.js
@@ -0,0 +1,13 @@
+const button = document.getElementById('btn');
+const toasts = document.getElementById('toasts');
+
+const messages = [
+  'Message One',
+  'Message Two',
+  'Message Three',
+  'Message Four',
+  'Message Five',
+  'Message Six'
+];
+
+button.addEventListener('click');
