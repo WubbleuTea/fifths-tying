6cb9b1db7f8eddc31626b30ce1085c68b58b63fd
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2021-01-25 07:15:40 -0600

    user card minus repos appends, error of no user handled

--- 28-github-profiles/script.js
+++ 28-github-profiles/script.js
@@ -2,14 +2,56 @@ const APIURL = 'https://api.github.com/users/';
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 async function getUser(username) {
   try {
     const { data } = await axios(APIURL + username);
-    console.log(data);
+    createUserCard(data);
   } catch (err) {
-    console.log(err);
+    if (err.response.status == 404) {
+      createErrorCard('No Profile With This Username');
+    }
   }
+}
+
+function createUserCard(user) {
+  const { avatar_url, name, bio, followers, following, public_repos } = user;
+  const cardHTML = `
+    <div class="card">
+      <div>
+        <img
+          src="${avatar_url}"
+          alt="${name}"
+          class="avatar"
+        />
+      </div>
+      <div class="user-info">
+        <h2>${name}</h2>
+        <p>${bio}</p>
+
+        <ul>
+          <li>${followers} <strong>Followers</strong></li>
+          <li>${following} <strong>Following</strong></li>
+          <li>${public_repos} <strong>Repos</strong></li>
+        </ul>
+
+        <div id="repos"></div>
+      </div>
+    </div>
+  `;
+
+  main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+    <div class="card">
+      <h1>${msg}</h1>
+    </div>
+  `;
+
+  main.innerHTML = cardHTML;
 }
 
 form.addEventListener('submit', e => {
@@ -21,3 +63,7 @@ form.addEventListener('submit', e => {
     search.value = '';
   }
 });
+
+{
+  /* <a href="#" class="repo">Repo 1</a> */
+}
